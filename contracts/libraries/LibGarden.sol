9e00fc991ff2d09ab4f0edcb3d6c30194c111029
Bennett Hoffman <benn.hoffman@gmail.com>
2022-03-27 21:56:24 -0400

    Master gardener address

--- contracts/libraries/LibGarden.sol
+++ contracts/libraries/LibGarden.sol
@@ -3,12 +3,15 @@ pragma solidity ^0.8.0;
 
 import {IGardenFactory} from "../interfaces/IGardenFactory.sol";
 import {IGardenRouter} from "../interfaces/IGardenRouter.sol";
+import {IMasterGardener} from "../interfaces/IMasterGardener.sol";
 
 library LibGarden {
     IGardenFactory public constant MAINNET_FACTORY =
         IGardenFactory(0x9014B937069918bd319f80e8B3BB4A2cf6FAA5F7);
     IGardenRouter public constant MAINNET_ROUTER =
         IGardenRouter(0x24ad62502d1C652Cc7684081169D04896aC20f30);
+
+    IMasterGardener public constant MAINNET_MASTER_GARDENER = IMasterGardener(0xDB30643c71aC9e2122cA0341ED77d09D5f99F924);
 
     enum Pool {
         JewelOne, // 0
